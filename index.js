38560ca179692f336518c5417024852461a1ca00
alexcahn <alexbcahn@gmail.com>
2019-08-09 06:49:58 -0500

    enter username required to chat

--- index.js
+++ index.js
@@ -11,35 +11,39 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
-app.get('/javascript', (req, res) => {
-    res.sendFile(__dirname + '/public/javascript.html')
+app.get('/hockey', (req, res) => {
+    res.sendFile(__dirname + '/public/views/hockey.html')
 })
 
-app.get('/swift', (req, res) => {
-    res.sendFile(__dirname + '/public/swift.html')
+app.get('/football', (req, res) => {
+    res.sendFile(__dirname + '/public/views/football.html')
 })
 
-app.get('/css', (req, res) => {
-    res.sendFile(__dirname + '/public/css.html')
+app.get('/baseball', (req, res) => {
+    res.sendFile(__dirname + '/public/views/baseball.html')
+})
+
+app.get('/basketball', (req, res) => {
+    res.sendFile(__dirname + '/public/views/basketball.html')
 })
 
 // tech namespace
-const tech = io.of('/tech');
+const sports = io.of('/sports');
 
-tech.on('connection', (socket) => {
+sports.on('connection', (socket) => {
     socket.on('join', (data) => {
         socket.join(data.room)
-        tech.in(data.room).emit('message', `New user joined ${data.room} room!`)
+        sports.in(data.room).emit('message', `${data.person} joined ${data.room} room!`)
     })
 
     socket.on('message', (data) => {
         console.log(`message: ${data.msg}`);
-        tech.emit('message', data.msg);
+        sports.emit('message', data.msg);
     });
 
     socket.on('disconnect', () => {
         console.log('user disconnected')
         // tell everybody someone disconnected
-        tech.emit('message', 'user disconnected')
+        sports.emit('message', 'user disconnected')
     })
 })
