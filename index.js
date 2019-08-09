51d19c9b87965b4152af967e46393d029aa54518
alexcahn <alexbcahn@gmail.com>
2019-08-09 17:22:19 -0500

    server to listen for heroku

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const port = 3000;
 
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 app.get('/', (req, res) => {
